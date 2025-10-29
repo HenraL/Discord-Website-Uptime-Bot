0be60ecbad136e5ae513ee4d85c6679e529eb131
Henry LETELLIER <henry.letellier@epitech.eu>
2025-10-29 01:56:47 +0100

    Create __init__.py
    Create __init__.py to allow an easy import for external programs

--- DiscordBot/src/__init__.py
+++ DiscordBot/src/__init__.py
@@ -4,9 +4,10 @@ Exports a small, convenience API for programmatic usage of the
 ``code_logic`` package (the program entrypoint and helpers).
 """
 
-from .code_logic import Main
+from .code_logic import Main, start_wrapper
 from .code_logic.program_globals import HLP
 __all__ = [
+    "HLP",
     "Main",
-    "HLP"
+    "start_wrapper"
 ]
