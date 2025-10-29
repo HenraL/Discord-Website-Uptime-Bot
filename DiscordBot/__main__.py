1e17e1a4f16b3da7cc24d121c1be7068cfecb9f9
Henry LETELLIER <henry.letellier@epitech.eu>
2025-10-29 01:54:30 +0100

    Refactor __main__.py
    Refactor __main__.py to have less possible failure points

--- DiscordBot/__main__.py
+++ DiscordBot/__main__.py
@@ -5,27 +5,37 @@ This module provides an entrypoint so the package can be executed with
 ``DiscordWebsiteMonitor`` module.
 """
 
-DWM = None
-V2 = None
+from functools import partial
+
+RUNER = None
+
 
 try:
-    import src.code_logic
-    V2 = src.code_logic.start_wrapper
+    import src
+    RUNNER = src.start_wrapper
 except ImportError as e:
     print(
-        f"Failed to import version 2 of the program, defaulting to v1, v2 import error: '{e}'"
+        f"Failed to import version 2 of the program, trying another import mode, v2 import error: '{e}'"
     )
     try:
-        import DiscordWebsiteMonitor
-        DWM = DiscordWebsiteMonitor
-    except ImportError as e2:
-        err: str = f"Could not find any versions of the program that could be started, v1 import error: {e2}"
-        print(err)
-        raise RuntimeError(err) from e2
+        from .src import start_wrapper
+        RUNNER = start_wrapper
+    except ImportError as error:
+        print(
+            f"Failed to import version 2 of the program, defaulting to v1, v2 import error: '{error}'"
+        )
+        try:
+            import DiscordWebsiteMonitor
+            RUNNER = partial(
+                DiscordWebsiteMonitor.client.run,
+                DiscordWebsiteMonitor.TOKEN
+            )
+        except ImportError as e2:
+            err: str = f"Could not find any versions of the program that could be started, v1 import error: {e2}"
+            print(err)
+            raise RuntimeError(err) from e2
 
 
 if __name__ == "__main__":
-    if V2:
-        V2()
-    if DWM:
-        DWM.client.run(DWM.TOKEN)
+    if RUNNER:
+        RUNNER()
